357e92273bdb8162fb77fd3c3fd4cf264b39bb1b
Ayilk <srangel.h92@gmail.com>
2022-06-11 01:30:36 -0500

    Modificando modelo Order y endpoint post Order

--- api/src/controllers/Order.js
+++ api/src/controllers/Order.js
@@ -2,12 +2,20 @@ const {Order, User} = require('../db');
 
 const postOrder = async(req, res, next) => {
     const { idUser } = req.params;
-    const { id, amount, orderIdPayment, email_address, paymentSource, status } = req.body
+    const { id, first_name, last_name, payer_Id, amount, cycles_completed,
+            cycles_remaining, sequence, ternure_type, total_cycles, failed_payments_count,
+            last_payment_time, next_billing_time, create_time, plan_Id, plan_overridden,
+            quantity, start_time, status, status_update_time, email_address
+          } = req.body
     try {
         const createdBy = await User.findByPk(idUser);
 
         const newOrder = await Order.create(
-            { id, amount, orderIdPayment, email_address, paymentSource, status }
+            { id, first_name, last_name, payer_Id, amount, cycles_completed,
+              cycles_remaining, sequence, ternure_type, total_cycles, failed_payments_count,
+              last_payment_time, next_billing_time, create_time, plan_Id, plan_overridden,
+              quantity, start_time, status, status_update_time, email_address
+            }
         )
 
         createdBy.addOrder(newOrder);
--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -7,12 +7,80 @@ module.exports = (sequelize) => {
       primaryKey: true,
       allowNull: false,      
     },
+    first_name: {
+      type: DataTypes.STRING,      
+      allowNull: true,      
+    },
+    last_name: {
+      type: DataTypes.STRING,      
+      allowNull: true,      
+    },
+    payer_Id: {
+      type: DataTypes.STRING,      
+      allowNull: true,      
+    },
     amount: {
       type: DataTypes.FLOAT,
-      allowNull: false,
+      allowNull: true,
       defaultValue: 0,
     },
-    orderIdPayment: {
+    cycles_completed: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    cycles_remaining: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    sequence: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    tenure_type: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    total_cycles: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    failed_payments_count: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },    
+    last_payment_time: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    next_billing_time: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    create_time: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    plan_Id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    plan_overridden: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    start_time: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    status_update_time: {
       type: DataTypes.STRING,
       allowNull: true,
     },
@@ -21,14 +89,6 @@ module.exports = (sequelize) => {
       allowNull: true,
       isEmail: true,
     },        
-    paymentSource: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },    
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "PENDING",
-    },
+    
   });
 };
