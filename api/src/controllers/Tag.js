23b15425efd0aff01c6d67963be341f76b26fbe5
reydez <icyreyeslod@gmail.com>
2022-06-13 17:06:41 -0500

    most used tags implementation

--- api/src/controllers/Tag.js
+++ api/src/controllers/Tag.js
@@ -1,53 +1,75 @@
-const { Tag, Module } = require('../db')
+const { Tag, Module } = require("../db");
 
-
-const getAllTags = async(req, res, next) => {
-    try {
-        const response = await Tag.findAll()
-        res.json(response)
-    } catch (error) {
-        next(error)
-    }
+const getAllTags = async (req, res, next) => {
+  try {
+    const response = await Tag.findAll();
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const adminAddTags = async(req, res, next) => {
-    const { tag } = req.body;
-    const { idModule } = req.params;
-    try {
-        if(!tag || !idModule) return res.status(400).send("Incomplete fields")
-        const module = await Module.findByPk(idModule);
-        if(!module) return res.status(404).send("Module not found")
-        const exist = await Tag.findAll({
-            where: { name: tag }
-        });
-        if(exist.length) return res.status(400).send("Tag ya existente!")
-        const newTag = await Tag.create({
-            name: tag
-        });
-        module.addTag(newTag)
-        res.json(newTag.id)
-    } catch (error) {
-        next(error)
-    }
+const adminAddTags = async (req, res, next) => {
+  const { tag } = req.body;
+  const { idModule } = req.params;
+  try {
+    if (!tag || !idModule) return res.status(400).send("Incomplete fields");
+    const module = await Module.findByPk(idModule);
+    if (!module) return res.status(404).send("Module not found");
+    const exist = await Tag.findAll({
+      where: { name: tag },
+    });
+    if (exist.length) return res.status(400).send("Tag ya existente!");
+    const newTag = await Tag.create({
+      name: tag,
+    });
+    module.addTag(newTag);
+    res.json(newTag.id);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const adminDeleteTags = async(req, res, next) => {
-    const { idTag } = req.params
-    try {
-        if(!idTag) return res.send("Incomplete fields");
-        const deleted = await Tag.destroy({
-            where: {
-                id: idTag
-            }
-        });
-        res.json({deleted: Boolean(deleted)})
-    } catch (error) {
-        next(error)
-    }
+const adminDeleteTags = async (req, res, next) => {
+  const { idTag } = req.params;
+  try {
+    if (!idTag) return res.send("Incomplete fields");
+    const deleted = await Tag.destroy({
+      where: {
+        id: idTag,
+      },
+    });
+    res.json({ deleted: Boolean(deleted) });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const plusOneTag = async (req, res, next) => {
+  const { idTag } = req.params;
+  const tagAModicar = await Tag.findByPk(idTag);
+  console.log(tagAModicar);
+  try {
+    if (!idTag) return res.send("Algo salio mal");
+    await Tag.update(
+      {
+        usado: tagAModicar.usado + 1,
+      },
+      {
+        where: {
+          id: idTag,
+        },
+      }
+    );
+    res.json({ message: "usado + 1" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    getAllTags,
-    adminAddTags, 
-    adminDeleteTags
-}
+  getAllTags,
+  adminAddTags,
+  adminDeleteTags,
+  plusOneTag,
+};
--- api/src/routes/tags.js
+++ api/src/routes/tags.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
-const { Router } = require('express');
+require("dotenv").config();
+const { Router } = require("express");
 const router = Router();
-const { getAllTags } = require('../controllers/Tag')
+const { getAllTags, plusOneTag } = require("../controllers/Tag");
 // const {} = require('../middleware');
 
-router.get('/', getAllTags);
+router.get("/", getAllTags);
+router.put("/mas-uno/:idTag", plusOneTag);
 
-module.exports = router
+module.exports = router;
